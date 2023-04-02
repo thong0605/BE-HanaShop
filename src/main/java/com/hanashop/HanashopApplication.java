afb4381e50e9146197abba966714f81572c666fc
thong0605 <thong0605@yahoo.com>
2023-04-02 16:35:13 +0700

    First Commit

---
+++ src/main/java/com/hanashop/HanashopApplication.java
@@ -0,0 +1,13 @@
+package com.hanashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanashop/HanashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
